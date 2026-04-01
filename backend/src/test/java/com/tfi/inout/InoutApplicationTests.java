36ee071aac49dec1ca0af7fcbeb8e9a25b73684c
Kevin <kfornier@gmail.com>
2026-03-31 23:26:01 -0300

    first

---
+++ backend/src/test/java/com/tfi/inout/InoutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tfi.inout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InoutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
