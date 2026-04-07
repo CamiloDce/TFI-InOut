5dbe48f84d982a41db80a25fe4d77a45aa0d8a23
Kevin <kfornier@gmail.com>
2026-04-07 20:01:13 -0300

    feat(controller): add controller for employee

--- backend/src/main/java/com/tfi/inout/controller/EmployeeController.java
+++ backend/src/main/java/com/tfi/inout/controller/EmployeeController.java
@@ -1,4 +1,45 @@
 package com.tfi.inout.controller;
 
+import com.tfi.inout.dto.EmployeeDto;
+import com.tfi.inout.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/employees")
 public class EmployeeController {
+    @Autowired
+    private EmployeeService employeeService;
+
+    @PostMapping
+    public ResponseEntity<String> createEmployee(@RequestBody EmployeeDto employeeDto) {
+        employeeService.createEmployee(employeeDto);
+        return ResponseEntity.ok("Employee successfully created");
+    }
+
+    @GetMapping("/list")
+    public List<EmployeeDto> listEmployees() {
+        return employeeService.list();
+    }
+
+    @GetMapping("/list/{id}")
+    public EmployeeDto getEmployee(@PathVariable Long id) {
+        return employeeService.listId(id);
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<String> updateEmployee(@PathVariable Long id,
+                                                 @RequestBody EmployeeDto employeeDto) {
+        employeeService.edit(id, employeeDto);
+        return ResponseEntity.ok("Employee successfully updated");
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> deleteEmployee(@PathVariable Long id) {
+        employeeService.delete(id);
+        return ResponseEntity.ok("Employee successfully deleted");
+    }
 }
