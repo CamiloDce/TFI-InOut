b491dd6876d67d92e948e48b0c706efe47c58785
CamiloDce <camilod0310@gmail.com>
2026-04-20 20:14:06 -0300

    actualizar

---
+++ frontend/src/app/login/login.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './login.html',
+  styleUrl: './login.css'
+})
+export class LoginComponent {
+
+  form: any;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  login() {
+    if (this.form.valid) {
+      const { username, password } = this.form.value;
+
+      if (username === 'admin' && password === '1234') {
+        alert('Login correcto');
+      } else {
+        alert('Credenciales incorrectas');
+      }
+    }
+  }
+}
